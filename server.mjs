365a65f3f7cd78ee9feda7d0a19b680a3b99cb29
Abdullahrazaali <92160968+Abdullahrazaali@users.noreply.github.com>
2021-11-04 22:56:09 +0500

    node

---
+++ server.mjs
@@ -0,0 +1,19 @@
+
+   
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("I am a hello world server");
+})
+app.get("/profile", (req, res) => {
+    res.send("here is your profile");
+})
+app.get("/about", (req, res) => {
+    res.send("new message for about");
+})
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
